926b17d10e149bf61fa1a91f927127f96c2c259e
Quang Nguyen <quangnguyenmail96@gmail.com>
2021-09-17 18:08:39 +0700

    delete all packages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,5 @@
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import About from "./components/About";
-import Shop from "./components/Shop";
 function App() {
-  return (
-    <>
-      <Router>
-        <ul>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/shop">shop</Link>
-          </li>
-        </ul>
-
-        <Switch>
-          <Route path="/about" component={About} />
-          <Route path="/shop" component={Shop} />
-        </Switch>
-      </Router>
-    </>
-  );
+  return <>Hello boilerplate</>;
 }
 
 export default App;
